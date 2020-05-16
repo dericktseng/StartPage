47fd4103e384ed429abbf3a399b990263d73889d
Derick Tseng <derickboss1@gmail.com>
2020-05-15 23:11:38 -0700

    first commit

---
+++ src/bookmarker/loader.js
@@ -0,0 +1,87 @@
+// name of the folder.
+var FOLDERNAME = "StartPage";
+var LOCATION = "Bookmarks Toolbar";
+
+/* Gets the correct tree to work with.
+ * @param tree - root tree.
+ * @return tree to display. */
+function getTree(tree) {
+    tree = tree[0];  // gets the tree root
+    var children = tree['children'];
+    for (child of children) {
+        if (child['title'] == LOCATION) {
+            tree = child;
+            children = tree['children'];
+            break;
+        }
+    }
+    for (child of children) {
+        if (child['title'] == FOLDERNAME) {
+            tree = child;
+            break;
+        }
+    }
+    return tree;
+}
+
+
+/* Checks whether given startpage folder exists. */
+function exists(tree) {
+    return (tree['title'] == FOLDERNAME);
+}
+
+
+/* Changes the given tree to an Object. */
+function objectOutput(bookmarks) {
+    var bookmarkobj = {};
+    var titleobj = {};
+    objectOutputHelper(bookmarks, bookmarkobj, titleobj);
+    return [bookmarkobj, titleobj];
+}
+
+/* Helper function for objectOutput. */
+function objectOutputHelper(bookmarks, bookmarkobj, titleobj) {
+    if (bookmarks['type'] == "bookmark") {
+        var key = bookmarks['parentId'];
+        bookmarkobj[key].push(bookmarks);
+    } else {
+        var key = bookmarks['id'];
+        bookmarkobj[key] = [];
+        titleobj[key] = bookmarks['title'];
+        for (child of bookmarks['children']) {
+            objectOutputHelper(child, bookmarkobj, titleobj);
+        }
+    }
+}
+
+/* Changes bookmarkObject to html. */
+function toHTML(tree) {
+    var obj = objectOutput(tree);
+    var foldernames = obj[1];
+    var bookmarks = obj[0];
+    console.log(foldernames);
+    console.log(bookmarks);
+}
+
+/* Displays given bookmarktree onto the screen.
+ * @param tree - the root tree.
+ */
+function display(tree) {
+    tree = getTree(tree);
+    if (exists(tree)) {
+        console.log(tree);
+        console.log(toHTML(tree));
+    } else {
+        browser.bookmarks.create({
+            "index": 0,
+            "parentId": tree['id'],
+            "title": FOLDERNAME,
+            "type": "folder"
+        });
+    }
+}
+
+// loads bookmarks from the root
+var bookmarktree = browser.bookmarks.getTree();
+bookmarktree.then(display, console.error);
+
